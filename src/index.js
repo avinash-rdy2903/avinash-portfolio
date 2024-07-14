c0e7bc548f8c6e851a6d1ebbd6d78144ad96fa5f
avinash-rdy2903 <mettukuruavinashreddy@gmail.com>
2024-07-14 03:31:20 -0400

    edited profile data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.js';
 import * as serviceWorker from './serviceWorker';
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* GLOBAL VARIABLES */
@@ -12,7 +13,10 @@ window.$secondaryLanguage = 'pl';
 window.$primaryLanguageIconId = 'primary-lang-icon';
 window.$secondaryLanguageIconId = 'secondary-lang-icon';
 
-
+if (process.env.REACT_APP_NODE_ENV === 'production') {
+    console.log("disable dev tools");
+    disableReactDevTools();
+  }
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
